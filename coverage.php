07154844870ddf13142f4168b0fc8b1d9caacbea
joshgreen <emailcoder@gmail.com>
2014-12-18 13:27:48 +0000

    finished up coverage page

---
+++ coverage.php
@@ -0,0 +1,103 @@
+<?php
+/**
+* Template name: Coverage
+
+ * The template for displaying all pages.
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site will use a
+ * different template.
+ *
+ * @package Michaelfilbey
+ */
+
+get_header(); ?>
+
+  <div id="primary" class="content-area">
+    <main id="main" class="site-main max" role="main">
+
+      <div class="coverage intro">
+        <div class="map">
+           <img src="<?php bloginfo('template_directory'); ?>/img/catchment.png" alt="Coverage map">
+        </div>
+        <div class="coverage-intro-text">
+          <h3>Coverage</h3>
+          <p>Michael is based in Uxbridge and as a rule of thumb looks to work within a 20-30 minute driving distance of Uxbridge. This is primarily to give him flexibility to get to emergency call outs quickly should the need arise. Using Uxbridge as the centre point the preferred work areas are Slough and Beaconsfield in the West, Amersham in the North, Ealing in the East and Heathrow and Hounslow in the South.</p>
+        </div>
+      </div>
+
+      <hr>
+
+      <div class="text-area-two">
+        <div>
+          <h3>
+            Hours and call out
+          </h3>
+          <p>Michaels normal working hours are 08:00 to 17:00 and he likes to arrive early and get straight on with the job at hand. Should a job require Michael to arrive earlier or stay later then he is very flexible and happy to try and accommodate your needs.</p>
+        </div>
+        <div>
+          <p>His emergency call out hours are Monday to Saturday 07:00 to 22:00 and emergency call out is likely to incur a small call out fee on top of the normal hourly rate, but call Michael to discuss.</p>
+        </div>
+      </div>
+
+      <hr>
+
+      <div class="text-area intro postcodes">
+        <div>
+          <h3>Postcodes</h3>
+          <ul>
+            <li>TW6</li>
+            <li>TW19</li>
+            <li>SL3</li>
+            <li>SL4</li>
+            <li>SL60</li>
+
+            <li>SL1</li>
+             </ul>
+          <ul>
+            <li>SL2</li>
+            <li>SL9</li>
+            <li>HA</li>
+            <li>HA1</li>
+
+            <li>HA10</li>
+            </ul>
+          <ul>
+            <li>UB6</li>
+            <li>HA9</li>
+            <li>HA5</li>
+            <li>HA6</li>
+
+            <li>UB9</li>
+             </ul>
+          <ul>
+            <li>UB7</li>
+            <li>W10</li>
+            <li>W3</li>
+            <li>W4</li>
+            <li>TW7</li>
+            <li>TW3</li>
+          </ul>
+        </div>
+      </div>
+
+      <hr>
+
+      <div class="text-area intro">
+        <div>
+          <h3>Partnership information</h3>
+          <p>Michael is a trusted maintenance man for many local estate agents, letting agents and multi property landlords. He provides them with a first class and speedy service. If you want to learn more about establishing a partnership with Michael get in touch by emailing <a href="mailto:mf_[at]_michaelspropertymaintainence.com" class="mail">mf_[at]_michaelspropertymaintainence.com</a></p>
+        </div>
+      </div>
+
+      <hr>
+
+
+
+
+
+    </main><!-- #main -->
+  </div><!-- #primary -->
+
+<?php get_footer(); ?>
